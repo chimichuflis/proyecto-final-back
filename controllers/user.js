dd234e0a736e092fb424380f70b561fb8e06afb2
H4rdan <javier-pereyra-14@hotmail.com>
2023-10-28 19:00:13 -0300

    correccion status servidor

--- controllers/user.js
+++ controllers/user.js
@@ -3,31 +3,31 @@ const bcrypt = require("bcryptjs");
 const jsonwebtoken = require("jsonwebtoken");
 
 
-const validEmail = async (req,res)=>{
-  try{
+const validEmail = async (req, res) => {
+  try {
     const user = await knex("users")
       .where("email", req.body.email)
       .first()
 
-    return res.json({validity: !user, msg: user?"email ya existe":"email disponible"});
+    return res.json({ validity: !user, msg: user ? "email ya existe" : "email disponible" });
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err);
   }
 }
 
 
-const userRegister = async (req,res)=>{
-  try{
+const userRegister = async (req, res) => {
+  try {
     const user = await knex("users")
       .where("email", req.body.email)
       .first();
 
-    if(user){
-      return res.status(409).json({pass: false, msg: "email already in use"});
+    if (user) {
+      return res.json({ pass: false, msg: "email already in use" });
     }
     const salt = await bcrypt.genSalt(10);
-    const passwordEncrypt = await bcrypt.hash(req.body.password,salt);
+    const passwordEncrypt = await bcrypt.hash(req.body.password, salt);
     await knex("users")
       .insert(
         {
@@ -37,30 +37,30 @@ const userRegister = async (req,res)=>{
           user_title: req.body.profile
         }
       );
-    return res.json({pass: true, msg:"user created succesfuly"});
+    return res.json({ pass: true, msg: "user created succesfuly" });
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err);
   }
 }
 
 
-const userLogin = async (req,res)=>{
-  try{
+const userLogin = async (req, res) => {
+  try {
     const user = await knex("users")
       .where("email", req.body.profile)
       .orWhere("user_name", req.body.profile)
       .first()
 
-    if(!user){
-      return res.status(409).json({pass: false, msg: "invalid user or password"});
+    if (!user) {
+      return res.json({ pass: false, msg: "invalid user or password" });
     }
-    
+
     const validatePassword = await bcrypt.compare(
       req.body.password, user.password
     );
-    if(!validatePassword){
-      return res.status(409).json({pass: false, msg: "invalid email or password"});
+    if (!validatePassword) {
+      return res.json({ pass: false, msg: "invalid email or password" });
     }
 
     const token = jsonwebtoken.sign(
@@ -68,11 +68,11 @@ const userLogin = async (req,res)=>{
         id: user.user_id,
         name: user.user_name,
         email: user.email
-      },"audn",{ expiresIn: '600000s' }
+      }, "audn", { expiresIn: '600000s' }
     );
-    return res.json({pass: true, msg:"token generated successfuly", token: token});
+    return res.json({ pass: true, msg: "token generated successfuly", token: token });
   }
-  catch(err){
+  catch (err) {
     res.status(400).json(err);
   }
 }
