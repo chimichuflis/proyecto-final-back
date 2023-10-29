a377844d896dc38e3749645d854c6c1eb63a1ab5
silver <silver@sildrid.com>
2023-10-29 17:55:13 -0300

    bugfixes

--- controllers/match.js
+++ controllers/match.js
@@ -1,5 +1,15 @@
-const getMatchArtists = (req,res)=>{
-  res.json("lol")
+const knex = require("../config/knexfile");
+const getMatchArtists = async (req,res)=>{
+  try{
+    const artists = await knex("artists")
+      .orderBy(knex.raw("random()"))
+      .limit(10)
+      return res.json({artists:artists});
+
+  }
+  catch(err){
+    return res.status(500).json({error: err});
+  }
 }
 
 module.exports = {getMatchArtists}
