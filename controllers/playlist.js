f25e04892e14300877467932707c7b233bad1bbd
tomas miranda <tmirandafrontan@gmail.com>
2023-10-31 22:51:04 -0300

    songs playlist

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -10,88 +10,100 @@ const getPlaylists = async (req, res) => {
     res.json(playlists);
     */
     const playlists = await knex("playlists_songs")
-      .join("playlists", "playlists.playlist_id", "=", "playlists_songs.playlist_id")
+      .join(
+        "playlists",
+        "playlists.playlist_id",
+        "=",
+        "playlists_songs.playlist_id"
+      )
       .join("songs", "songs.song_id", "=", "playlists_songs.song_id")
+      .select("playlists_songs.playlist_id", "playlist_name", "artist_id")
+      .where("user_id", req.user.id)
+      .distinctOn("playlists_songs.playlist_id");
+
+    return res.json(playlists);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+const getPlaylistSongs = async (req, res) => {
+  try {
+    const playlistSongs = await knex("playlists_songs")
+      .join(
+        "playlists",
+        "playlists.playlist_id",
+        "=",
+        "playlists_songs.playlist_id"
+      )
+      .join("songs", "songs.song_id", "=", "playlists_songs.song_id")
+      .join("genres", "genres.genre_id", "=", "songs.genre_id")
+      .join("artists", "songs.artist_id", "=", "artists.artist_id")
       .select(
-        "playlists_songs.playlist_id",
-        "playlist_name",
-        "artist_id"
+        "songs.song_id",
+        "song_name",
+        "album_name",
+        "song_duration",
+        "genre_name",
+        "artist_name",
+        "artists.artist_id"
       )
       .where("user_id", req.user.id)
-      .distinctOn("playlists_songs.playlist_id")
-
-    return res.json(playlists)
+      .andWhere("playlists_songs.playlist_id", req.params.id);
+    return res.json(playlistSongs);
+  } catch (err) {
+    return res.status(500).json(err);
   }
-  catch (error) {
-      res.status(500).json({ message: error });
-  }
-}
+};
 
-const getPlaylistSongs = async(req,res)=>{
-  try{
-      const playlistSongs = await knex("playlists_songs")
-        .join("playlists", "playlists.playlist_id", "=", "playlists_songs.playlist_id")
-        .join("songs", "songs.song_id", "=", "playlists_songs.song_id")
-        .join("genres", "genres.genre_id", "=", "songs.genre_id")
-        .join("artists", "songs.artist_id", "=", "artists.artist_id")
-        .select("songs.song_id","song_name", "album_name", "song_duration", "genre_name", "artist_name")
-        .where("user_id",req.user.id)
-        .andWhere("playlists_songs.playlist_id", req.params.id)
-      return res.json(playlistSongs)
-  }
-  catch(err){
-    return res.status(500).json(err)
-  }
-}
-
-
-const createPlaylist = async (req,res) => {
-  try{
+const createPlaylist = async (req, res) => {
+  try {
     const newPlaylist = await knex("playlists")
-      .insert(
-        {
-          user_id: req.user.id,
-          playlist_name: req.body.name
-        }
-      ).returning("*")
-    res.json({pass:true, msg:"playlist created succesfuly",newPlaylist:newPlaylist[0].playlist_id});
+      .insert({
+        user_id: req.user.id,
+        playlist_name: req.body.name,
+      })
+      .returning("*");
+    res.json({
+      pass: true,
+      msg: "playlist created succesfuly",
+      newPlaylist: newPlaylist[0].playlist_id,
+    });
+  } catch (err) {
+    res.status(500).json({ pass: false, msg: err });
   }
-  catch(err){
-    res.status(500).json({pass: false, msg: err});
-  }
-}
+};
 
-const renamePlaylist = async (req, res)=>{
- try{
+const renamePlaylist = async (req, res) => {
+  try {
     await knex("playlists")
-      .where("playlist_id",req.body.id)
+      .where("playlist_id", req.body.id)
       .andWhere("user_id", req.user.id)
-      .update(
-        {
-          playlist_name: req.body.name
-        }
-      )
-      res.json({pass: true, msg: "playlist name updated"});
+      .update({
+        playlist_name: req.body.name,
+      });
+    res.json({ pass: true, msg: "playlist name updated" });
+  } catch (err) {
+    res.status(500).json({ pass: false, msg: err });
   }
-  catch(err){
-    res.status(500).json({pass: false, msg: err});
-  }
-}
+};
 
-const playlistAddSong = async(req,res)=>{
-  try{
-    await knex("playlists_songs")
-      .insert(
-        {
-          playlist_id: req.body.playlistId,
-          song_id: req.body.songId
-        }
-      )
-      res.json({pass: true, msg: "song inserted succesfuly"});
+const playlistAddSong = async (req, res) => {
+  try {
+    await knex("playlists_songs").insert({
+      playlist_id: req.body.playlistId,
+      song_id: req.body.songId,
+    });
+    res.json({ pass: true, msg: "song inserted succesfuly" });
+  } catch (err) {
+    res.status(500).json({ pass: false, msg: err });
   }
-  catch(err){
-    res.status(500).json({pass: false, msg: err});
-  }
-}
+};
 
-module.exports = { getPlaylists, getPlaylistSongs, createPlaylist, renamePlaylist, playlistAddSong }
+module.exports = {
+  getPlaylists,
+  getPlaylistSongs,
+  createPlaylist,
+  renamePlaylist,
+  playlistAddSong,
+};
--- routes/songsRoutes.js
+++ routes/songsRoutes.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const {tokenValidator} = require("../middleweares/validators/tokenValidator");
+const { tokenValidator } = require("../middleweares/validators/tokenValidator");
 
-const {
-    getSong,
-    findSongs
-} = require("../controllers/songs");
+const { getSong, findSongs } = require("../controllers/songs");
 
 router.get(
-    "/song/:songId",    // returns { }
-    tokenValidator,     // { song_id, song_name, album_name , song_duration , artist_name, genre_name }
-    getSong
+  "/song/:songId", // returns { }
+  tokenValidator, // { song_id, song_name, album_name , song_duration , artist_name, genre_name }
+  getSong
 );
 
-router.post(              // req.body = { value: "" }
-  "/song/search",         // returns { }
-  findSongs               // { songs: [], artists: [], albums: [] }
+router.post(
+  // req.body = { value: "" }
+  "/song/search", // returns { }
+  findSongs // { songs: [], artists: [], albums: [] }
 );
 
 module.exports = router;
