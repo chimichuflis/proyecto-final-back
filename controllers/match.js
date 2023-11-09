ed9ce06dad4bb003d6f408140f1cde375f42c9da
silver <silver@sildrid.com>
2023-11-08 23:38:44 -0300

    finalizado cupido musical

--- controllers/match.js
+++ controllers/match.js
@@ -17,7 +17,41 @@ const createMatchPlaylist = async (req,res)=>{
   const myArray = req.body.artists;
   try{
       const arr = await knex.raw('select * from songs where artist_id in (' + myArray.map(_ => '?').join(',') + ')', [...myArray]);
-    return res.json(arr.rows);
+      if(arr.rows.length==0){
+        return res.json({pass:false, msg:"no songs found"});
+      }
+      const newPlaylist = await knex("playlists")
+        .insert(
+        {
+        user_id: req.user.id,
+          playlist_name: "playlist de cupido musical"
+        }
+      ).returning("*")
+    console.log("playlist created ", newPlaylist);
+
+    //---------------------------------------------
+  
+    const insertQuery = [];
+    arr.rows.forEach((item,index)=>{
+      const itemPos = arr.rows.findIndex(n=>{
+        return n.song_id==item.song_id;
+      })
+      if(index == itemPos){
+        insertQuery.push({
+          song_id: item.song_id,
+          playlist_id: newPlaylist[0].playlist_id
+        });
+      }
+    });
+    console.log(insertQuery);
+
+  //--------------------------------------------------
+  
+    const addingSongs = await knex("playlists_songs")
+      .insert(insertQuery)
+
+    return res.json({pass: true, playlist_id:newPlaylist[0].playlist_id, msg: "playlist generated succesfully"});
+
   }
   catch(err){
     res.json({pass:false,err:err});
